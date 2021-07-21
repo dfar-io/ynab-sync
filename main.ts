12ef0872dd7d52ce85d24643513687d14ccc7b42
Dave Farinelli <d@dfar.io>
2021-07-21 13:54:38 +0000

    Conversion to scripts/GA schedule

---
+++ main.ts
@@ -0,0 +1,39 @@
+const { webkit } = require('playwright');
+
+if (process.argv.length != 4) {
+  console.error('usage: node main.ts <username> <password>');
+  process.exit(1);
+}
+
+(async () => {
+  const browser = await webkit.launch();
+  const page = await browser.newPage();
+  await page.goto('https://www.webpmt.com/cgi-bin/customers/clogin.pl');
+
+  // Login
+  const username = process.argv[2];
+  const password = process.argv[3];
+  await page.fill('#loannumber', username);
+  await page.fill('#password', password);
+  await page.press('#password', 'Enter');
+
+  // Move to My Info page
+  try {
+    await page.click('text=MY INFO');
+  } catch (e) {
+    if (e.name === 'TimeoutError') {
+      console.error('Unable to progress to \'My Info\' page, credentials may be bad.');
+      process.exit(1);
+    }
+  }
+
+  // Get loan balance
+  const selector = 'td.dcolor';
+  await page.waitForSelector(selector);
+  var elements = await page.$$(selector);
+  const loanBalance = await page.evaluate(el => el.innerText, elements[10]);
+  console.log(`Mortgage loan balance is: ${loanBalance}`);
+
+  await page.screenshot({ path: `example.png` });
+  await browser.close();
+})();
