d097c31ed5a08304fe8ee0e1ec5dc4b74eb139f2
Dave Farinelli <d@dfar.io>
2023-11-11 16:45:27 +0000

    Update home value to Realtor.com

--- get_home_value.js
+++ get_home_value.js
@@ -13,13 +13,12 @@ if (process.argv.length != 2) {
     recordVideo: { dir: 'video' }
   })
   const page = await context.newPage();
-  //const url = process.env.HOME_URL;
-  const url = 'https://www.redfin.com/MI/West-Bloomfield/2258-Holton-Ln-48323/home/93862019';
+  const url = 'https://www.realtor.com/realestateandhomes-detail/2258-Holton-Ln_West-Bloomfield_MI_48323_M36292-77663';
 
   try {
     await page.goto(url);
 
-    const homeValue = await page.evaluate(el => el.innerText, await page.$('div.statsValue > span'));
+    const homeValue = await page.evaluate(el => el.innerText, await page.$('.Pricestyles__StyledPrice-rui__btk3ge-0.bvgLFe'));
     console.log(homeValue.replace('$', '').replace(',', ''));
   } catch (err) {
     await context.close();
