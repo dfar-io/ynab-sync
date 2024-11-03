ad5d7ad0bcaacb46388f76083df1aad655a4e0c0
Dave Farinelli <d@dfar.io>
2024-11-03 10:41:29 -0500

    Move API key to env var (#22)

--- get_car_value.js
+++ get_car_value.js
@@ -3,14 +3,14 @@
 
 import fetch from 'node-fetch';
 
-if (process.argv.length != 4) {
-  console.error('usage: node get_car_balance.js <api_key> <vin>');
+if (process.argv.length != 3) {
+  console.error('usage: node get_car_balance.js <vin>');
   process.exit(1);
 }
 
 (async () => {
-  const api_key = process.argv[2];
-  const vin = process.argv[3];
+  const api_key = process.env.CAR_API_KEY;
+  const vin = process.argv[2];
 
   const response = await fetch(`https://api.carsxe.com/marketvalue?key=${api_key}&vin=${vin}`);
   const responseJson = await response.json();
